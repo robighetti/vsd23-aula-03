27277bed46be2362ba4993e1f580ad6294acb4f7
Rodrigo Bighetti <robighetti@gmail.com>
2023-04-20 08:54:00 -0300

    aula do dia 20/4

--- src/controllers.js
+++ src/controllers.js
@@ -19,29 +19,92 @@ const products = [
   }
 ]
 
+const userCart = []
+
 module.exports = {
   async createCart(request, response) {
 
     const { id, descricao, valor, qtde } = request.body
 
     const existsProduct = products.find(product => product.id === id)
-
-    console.log(existsProduct)
     if (existsProduct) {
       return response.status(400).json({
         error: 'Produto já cadastrado'
       })
     }
 
-    //Adicionar um novo elemento no array, usamos o push
-    products.push({
+    const existsDescription = products.find(product => product.descricao.toLocaleLowerCase() === descricao.toLocaleLowerCase())
+    if (existsDescription) {
+      return response.status(400).json({
+        error: 'Produto já cadastrado'
+      })
+    }
+
+    const product = {
       id,
       descricao,
       valor,
       qtde
-    })
+    }
 
+    //Adicionar um novo elemento no array, usamos o push
+    products.push(product)
 
+    return response.json({ data: product })
+  },
+
+  async getListOfCart(request, response) {
     return response.json({ data: products })
   },
+
+  async createUserCart(request, response) {
+
+    const { item } = request.body
+
+    for (const product of item) {
+      const productExists = products.find(prd => prd.id === product.id)
+
+      //Verifica se existe o produto
+      if (!productExists) {
+        return response.status(400).json({
+          error: 'Produto não encontrado'
+        })
+      }
+
+      //Verifica se possui estoque
+      if (product.qtde > productExists.qtde) {
+        return response.status(400).json({
+          error: 'Quantidade não disponivel'
+        })
+      }
+
+      const userItems = {
+        productId: productExists.id,
+        descricao: productExists.descricao,
+        qtde: product.qtde,
+        valor: product.qtde * productExists.valor
+      }
+
+      userCart.push(userItems)
+
+      //Diminui a quantidade em estoque
+      const index = products.findIndex(idx => idx.id === product.id)
+      products[index].qtde = products[index].qtde - product.qtde
+      //products[index].qtde -= product.qtde -> decremento
+      //products[index].qtde += product.qtde -> incremento
+    }
+
+    return response.json({
+      order: userCart,
+      stock: products
+    })
+
+    /* products.forEach((product, index) => {
+      console.log(product)
+    }) */
+
+    /* products.map((product, index) => {
+      console.log(index)
+    }) */
+  }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 const { Router } = require('express')
 
-const { createCart } = require('./controllers')
+const { createCart, getListOfCart, createUserCart } = require('./controllers')
 
 const routes = Router()
 
 routes.post('/cart', createCart)
+
+routes.get('/cart', getListOfCart)
+
+routes.post('/cart/user-cart', createUserCart)
 
 module.exports = routes
